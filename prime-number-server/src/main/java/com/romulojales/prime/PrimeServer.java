0efc43a408164230b8b2be466407e96332a8a56b
Rômulo Jales <romulo@romulojales.com>
2020-10-07 00:08:35 +0200

    Adding log in the grpc server

--- prime-number-server/src/main/java/com/romulojales/prime/PrimeServer.java
+++ prime-number-server/src/main/java/com/romulojales/prime/PrimeServer.java
@@ -2,18 +2,25 @@ package com.romulojales.prime;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
+
 public class PrimeServer {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PrimeServer.class);
 
+    public static void main(String[] args) throws IOException, InterruptedException {
+        LOGGER.info("Starting the GRPC server");
         Server server = ServerBuilder.forPort(9999)
                 .addService(new PrimeServerImpl())
                 .build();
 
         server.start();
+        LOGGER.info("The GRPC server has been started");
         server.awaitTermination();
+        LOGGER.info("The GRPC server has been terminated");
     }
 }
